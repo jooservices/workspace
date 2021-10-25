de466695a85e1f047bb7767a5fb779e259535c66
Viet Vu <jooservices@gmail.com>
2021-10-25 15:44:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
-#!/bin/bash  
+#!/bin/bash
 
 # Variables
-currentUser=$(whoami)
+OS_TYPE=""
+
+case $(uname -m) in
+    i386)   OS_TYPE="386" ;;
+    i686)   OS_TYPE="386" ;;
+    x86_64) OS_TYPE="amd64" ;;
+    arm)    dpkg --print-architecture | grep -q "arm64" && OS_TYPE="arm64" || OS_TYPE="arm" ;;
+    armv7l) OS_TYPE="armv7l"
+esac
+
+echo $OS_TYPE;
 
 echo Update
 sudo apt update && sudo apt -y upgrade
@@ -21,12 +31,15 @@ echo Mariadb
 sudo apt -y install mariadb-server
 
 echo Install PHP
-# For Pi 4
-sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-# For Ubuntu
-# sudo add-apt-repository ppa:ondrej/php && sudo apt update
-sudo apt update
+if [ "$OS_TYPE" = "armv7l" ]; then
+        sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+        echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+else
+        echo Ubuntu
+        sudo add-apt-repository ppa:ondrej/php
+fi
+
+sudo apt update && sudo apt upgrade
 sudo apt install -y php-dev
 sudo apt -y install php-imagick php-gd php-cli php-mbstring php-pecl-http php-uploadprogress imagemagick \
 php8.0-apcu php8.0-cli php8.0-curl php8.0-dev php8.0-gd php8.0-http php8.0-igbinary php8.0-imagick php8.0-intl php8.0-mbstring php8.0-mcrypt php8.0-oauth php8.0-pcov php8.0-raphf php8.0-soap php8.0-xml php8.0-xmlrpc php8.0-xsl php8.0-yaml php8.0-zip
@@ -57,6 +70,11 @@ echo Install Docker
 
 # sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+if [ "$OS_TYPE" = "armv7l" ]; then
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sudo sh get-docker.sh
+else
+        echo Ubuntu
+
+fi
 sudo usermod -aG docker $USER
